59272f2e5c42116f8025e52b304e4716c209bd51
Sebastian Mora <sebmora1055@gmail.com>
2018-07-20 15:15:34 -0700

    Updated Scraper but with bug
    Bug: Only gets tweets from its own account and does not terminate when reaching the end of the timeline.
    As each tweet is generated it is saved to a CSV

--- meme_scraper.py
+++ meme_scraper.py
@@ -8,6 +8,7 @@ This is a temporary script file.
 import tweepy as tweepy
 import time 
 import os
+import csv
 
 
 # Consumer keys and access tokens, used for OAuth
@@ -25,9 +26,54 @@ api = tweepy.API(auth)
 
 #api.update_status('Im back')
 
+
+
+def save_cvs(data):
+	with open("tweets.csv", 'wb') as  csv_file:
+		writer = csv.writer(csv_file)
+		writer.writerow(['id','created_at','text'])
+		writer.writerows(data)
+
+
 #gest n amount of tweets from the given username
-def get_tweets(username, n): 
+def get_tweets(screen_name, n): 
+	
+	alltweets = []
+
+	tl_tweets = api.user_timeline(username = '@seb1055',count=n) #get most recent tweets
+
+	alltweets.extend(tl_tweets) #save the most recent 
+
+	oldest = alltweets[-1].id - 1
+
+	while len(tl_tweets) > 0:
+		print "getting tweets before %s" % (oldest)
+		
+		#all subsiquent requests use the max_id param to prevent duplicates
+		new_tweets = api.user_timeline(username = '@seb1055',count=n,max_id=oldest)
+		
+		#save most recent tweets
+		alltweets.extend(new_tweets)
+		
+		#update the id of the oldest tweet less one
+		oldest = alltweets[-1].id - 1
+		
+		print "...%s tweets downloaded so far" % (len(alltweets))
+		save_cvs([[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets])
+	
+	#transform the tweepy tweets into a 2D array that will populate the csv	
+	
+
+
+
+
+
+def main():
+	get_tweets("@seb1055",200)
+	pass
     
-    tweets = api.user_timeline(screen_name=username)
+    
 
 
+if __name__ == "__main__":
+    main()
