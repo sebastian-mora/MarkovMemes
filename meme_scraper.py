9f9f804c6d8ec11e28dd40eb6bff07ed2ab05875
Manuel Vassos <mvassos@ucsc.edu>
2018-07-19 22:40:07 -0700

    Creating the scriprt to read tweets

---
+++ meme_scraper.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import tweepy as tweepy
+import time 
+import os
+
+
+# Consumer keys and access tokens, used for OAuth
+consumer_key = 'csVjaKZLVGSNbg0ublfPpw7vd'
+consumer_secret = 'XiixLm5g1ACEvQk0JPo5KllW393xDhy7aHK6vTlAJ0KUynw0mS' 
+access_token = '4846250833-OsRTKTZcTTXS9raSlHlrGYkeCF9BsaPeTLvFv9o'
+access_secret = '9TpUnT0GgEYCK9yI2KZ7Yh4bNLYIDyHwoDBmcIUxKOtiV'
+
+#OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+ 
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
+
+#api.update_status('Im back')
+
+def get_tweets
+
+
