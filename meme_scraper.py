b95a606264da512d47359d175434a5de58ecf5e5
Manuel Vassos <mvassos@ucsc.edu>
2018-07-21 16:09:47 -0700

    created filter and write tweets function, tweets.txt is full of data now

--- meme_scraper.py
+++ meme_scraper.py
@@ -55,13 +55,55 @@ def get_tweets(username, n):
 	alltweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
 	save_cvs(alltweets,username)
 
-
-
+#writes the necesary data from .csv file into txt format, isolates text information and removes RTs, etc.
+#writes the tweets into file named "tweets" in Scraped_TLS folder
+def filter_and_write_tweets(account):
+    
+    text_file = open("Scraped_TLS/tweets.txt", "w")
+    
+    if os.path.isfile("Scraped_TLS/tweets_%s.csv" %account) is False:
+        print("This account's file does not exist.")
+        return None
+    
+    else:
+        with open(("Scraped_TLS/tweets_%s.csv" %account), 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            
+            next(csv_reader) #skips format line
+            
+            for line in csv_reader:
+                
+                tweet = line[2]
+               #get rid of RTs
+                rt_check = tweet[:2]
+                if rt_check == 'RT':
+                    next(csv_reader)
+                    continue
+                
+                #ignore links and @s by breaking up each word in tweet
+                seperate_words = tweet.split(' ')
+                
+                for word in seperate_words:
+                    if ('https://' or 'http://') in word:
+                        seperate_words.remove(word)
+                        continue
+                    if ('@') in word:
+                        seperate_words.remove(word)
+                        continue
+                #join list back together into printable form
+                
+                tweet = " ".join(seperate_words)
+                text_file.write(tweet +"\n")
+     
+    text_file.close()   
+    
 
 def main():
 
 
-	accounts_tobe_scraped = ['@meatymadison', '@WORLDSTAR',]
+	accounts_tobe_scraped = ['@meatymadison', '@WORLDSTAR','@CueBallClarence',
+                          '@WoodySee', '@YLSMJ', '@hornystonerdad',
+                          '@ilooklikelilbil',]
 
 	for account in accounts_tobe_scraped:
 		if os.path.isfile("Scraped_TLS/tweets_%s.csv" %account) is False:
@@ -70,7 +112,7 @@ def main():
 		else:
 			print("%s already exists in the Archive" %account)
     
-    
+        filter_and_write_tweets(account)
 
 
 if __name__ == "__main__":
